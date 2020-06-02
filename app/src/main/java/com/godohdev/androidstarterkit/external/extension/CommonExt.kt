e172165a9b815cbd16c79a05cbc6743c5f3aaa93
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-02 15:01:31 +0700

    refactor(common-ext): add extention for lifecycle

--- app/src/main/java/com/godohdev/androidstarterkit/external/extension/CommonExt.kt
+++ app/src/main/java/com/godohdev/androidstarterkit/external/extension/CommonExt.kt
@@ -2,6 +2,9 @@ package com.godohdev.androidstarterkit.external.extension
 
 import android.content.Context
 import android.net.ConnectivityManager
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 
 /**
  *
@@ -14,4 +17,8 @@ fun Context.isNetworkConnected(): Boolean {
     val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val activeNetworkInfo = connectivityManager.activeNetworkInfo
     return activeNetworkInfo != null && activeNetworkInfo.isConnected
+}
+
+fun <T> LifecycleOwner.observe(liveData: LiveData<T>, action: (t: T) -> Unit) {
+    liveData.observe(this, Observer { it?.let { t -> action(t) } })
 }
