6ed4d4e0b94871ba0223d31ecdc5679ada098da4
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-02 13:18:44 +0700

    feat(constants): create constants to store app session constants

---
+++ app/src/main/java/com/godohdev/androidstarterkit/external/Constants.kt
@@ -0,0 +1,12 @@
+package com.godohdev.androidstarterkit.external
+
+/**
+ *
+ * Created by Wahyu Permadi on 02/06/20.
+ * Android Engineer
+ *
+ **/
+
+object AppSessionConstants {
+    const val AUTH_TOKEN = "auth_token"
+}
