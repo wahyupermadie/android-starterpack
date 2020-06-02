f140700a2928c9dbd8323b14d88b614067972e69
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-02 14:39:06 +0700

    feat(activity-scope): add ne wscoping

---
+++ app/src/main/java/com/godohdev/androidstarterkit/di/scope/ActivityScope.kt
@@ -0,0 +1,14 @@
+package com.godohdev.androidstarterkit.di.scope
+
+import javax.inject.Scope
+
+/**
+ *
+ * Created by Wahyu Permadi on 02/06/20.
+ * Android Engineer
+ *
+ **/
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ActivityScope
