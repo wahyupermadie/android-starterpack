509d9cfabbdc2751ae867fd3bb194a2a0ede2365
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-02 13:19:13 +0700

    feat(network-interceptor): create network interceptor to add header

--- app/src/main/java/com/godohdev/androidstarterkit/external/network/NetworkInterceptor.kt
+++ app/src/main/java/com/godohdev/androidstarterkit/external/network/NetworkInterceptor.kt
@@ -1,5 +1,9 @@
 package com.godohdev.androidstarterkit.external.network
 
+import com.godohdev.androidstarterkit.data.local.sdf.AppSession
+import okhttp3.Interceptor
+import okhttp3.Response
+
 /**
  *
  * Created by Wahyu Permadi on 02/06/20.
@@ -7,4 +11,27 @@ package com.godohdev.androidstarterkit.external.network
  *
  **/
 
-class NetworkInterceptor ()
+class NetworkInterceptor(private val appSession: AppSession) : Interceptor {
+    private val token: String?
+        get() = appSession.getTokenAuth()
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        token?.let {
+            request = if (it.isNotEmpty()) {
+                val finalToken = "Bearer $token"
+                val lOriginalRequest = chain.request()
+                val lRequest = lOriginalRequest.newBuilder()
+                    .header("Authorization", finalToken)
+                    .method(lOriginalRequest.method, lOriginalRequest.body).build()
+                lRequest
+            } else {
+                val lOriginalRequest = chain.request()
+                val lRequest = lOriginalRequest.newBuilder()
+                    .method(lOriginalRequest.method, lOriginalRequest.body).build()
+                lRequest
+            }
+        }
+        return chain.proceed(request)
+    }
+}
