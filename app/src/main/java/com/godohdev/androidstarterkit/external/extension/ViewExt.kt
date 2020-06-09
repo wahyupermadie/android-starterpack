0d48f8a29ee5239950c56a49bd589999ab8ca8cb
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-10 00:25:53 +0700

    refactor(view-ext): add set margin extension

--- app/src/main/java/com/godohdev/androidstarterkit/external/extension/ViewExt.kt
+++ app/src/main/java/com/godohdev/androidstarterkit/external/extension/ViewExt.kt
@@ -1,7 +1,10 @@
 package com.godohdev.androidstarterkit.external.extension
 
 import android.app.Activity
+import android.content.Context
+import android.util.TypedValue
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
@@ -26,4 +29,24 @@ fun Fragment.showSnackbar(snackbarText: String, timeLength: Int) {
 
 fun Activity.showSnackbar(snackbarText: String, timeLength: Int) {
     Snackbar.make(this.findViewById(android.R.id.content), snackbarText, timeLength).show()
+}
+
+fun View.setMargins(
+    leftMarginDp: Int? = null,
+    topMarginDp: Int? = null,
+    rightMarginDp: Int? = null,
+    bottomMarginDp: Int? = null
+) {
+    if (layoutParams is ViewGroup.MarginLayoutParams) {
+        val params = layoutParams as ViewGroup.MarginLayoutParams
+        leftMarginDp?.run { params.leftMargin = context.dpToPx(this) }
+        topMarginDp?.run { params.topMargin = context.dpToPx(this) }
+        rightMarginDp?.run { params.rightMargin = context.dpToPx(this) }
+        bottomMarginDp?.run { params.bottomMargin = context.dpToPx(this) }
+        requestLayout()
+    }
+}
+
+fun Context.dpToPx(number: Int): Int {
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, number.toFloat(), resources.displayMetrics).toInt()
 }
