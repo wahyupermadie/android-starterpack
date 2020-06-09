49d335bc743e5ca0e35b1cb8c359a742e8bc1459
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2020-06-10 00:36:38 +0700

    feat(resource): create resource for handle network state

---
+++ app/src/main/java/com/godohdev/androidstarterkit/data/Resource.kt
@@ -0,0 +1,42 @@
+package com.godohdev.androidstarterkit.data
+
+/**
+ *
+ * Created by Wahyu Permadi on 10/06/20.
+ * Android Engineer
+ *
+ **/
+
+data class Resource<T>(var status: Status, var data: T?, var error: Throwable?) {
+    companion object {
+        fun <T> success(data: T?): Resource<T> {
+            return Resource(
+                Status.SUCCESS,
+                data,
+                null
+            )
+        }
+
+        fun <T> error(error: Throwable, data: T?): Resource<T> {
+            return Resource(
+                Status.ERROR,
+                data,
+                error
+            )
+        }
+
+        fun <T> loading(data: T?): Resource<T> {
+            return Resource(
+                Status.LOADING,
+                data,
+                null
+            )
+        }
+    }
+
+    enum class Status {
+        SUCCESS,
+        ERROR,
+        LOADING
+    }
+}
